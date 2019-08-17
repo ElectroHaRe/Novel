4884f572b67d3f9c2e2caf2020ee5c69b93dd0be
Korolev Alexandr <hare1496@mail.ru>
2019-08-17 21:35:32 +0300

    SearchBox

---
+++ Editor/NovelFormsLibrary/NovelFormLibrary/SearchBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace NovelFormLibrary
+{
+    public partial class SearchBox : UserControl
+    {
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler OnSearchButtonClick;
+        public new string Text => requestBox.Text;
+
+        public int[] GetRequestIndexFrom(params string[] source)
+        {
+            int[] result = new int[0];
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (Regex.IsMatch(source[i], "(" + requestBox.Text + ")" + "+"))
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[result.Length - 1] = i;
+                }
+            }
+            return result;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            OnSearchButtonClick?.Invoke(sender, e);
+        }
+    }
+}
